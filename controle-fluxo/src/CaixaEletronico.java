2d281feecf816fcb17d8a2417a5be625e2130ab9
VinnyMendess <vinimendesdasilvaa@gmail.com>
2025-03-24 18:47:32 -0300

    subindo

---
+++ controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,21 @@
+public class CaixaEletronico{
+
+    public static void main (String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+        
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+
+            System.out.println(saldo);
+        }
+
+
+        //Condição ternária
+        int nota = 7;
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+
+    }
+}
---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+
+    public static void main ( String[] args) {
+
+        String alunos  [] = {"João", "Maria", "Pedro", "Ana", "Carlos"};
+
+        for (int x = 0; x < alunos.length; x++) {
+
+            System.out.println("O aluno no indice x = "+ x + " é " + alunos[x]);
+        }
+    }}
